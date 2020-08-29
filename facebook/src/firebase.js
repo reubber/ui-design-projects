ba8a4b38a03c1fde82e192d1403bd2b0f6b015ed
Reubber <reubbers@alu.ufc.br>
2020-08-28 22:33:17 -0300

    add firebase config

---
+++ facebook/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBocpo4gcUslCFnOQHWFpe82MC_3RdEPOE",
+  authDomain: "facebook-clone-5f273.firebaseapp.com",
+  databaseURL: "https://facebook-clone-5f273.firebaseio.com",
+  projectId: "facebook-clone-5f273",
+  storageBucket: "facebook-clone-5f273.appspot.com",
+  messagingSenderId: "223632671638",
+  appId: "1:223632671638:web:20f0194212ea2ebe5fd3b7",
+  measurementId: "G-89PBYF3KMN"
+};
