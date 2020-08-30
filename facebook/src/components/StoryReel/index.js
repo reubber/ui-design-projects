811714cd3cc73699571bc3d31f63cb37256b1aa0
Reubber <reubbers@alu.ufc.br>
2020-08-29 21:51:01 -0300

    add story components

---
+++ facebook/src/components/StoryReel/index.js
@@ -0,0 +1,38 @@
+import React from 'react'
+
+import './styles.css'
+import Story from '../Story'
+
+function StoryReel() {
+  return (
+    <div className="storyReel">
+      <Story
+        image="https://i.pinimg.com/236x/40/4e/67/404e677b0001979034a51f0065314143--cell-jr-modern-wallpaper.jpg"
+        profileSrc="https://storage.investagrams.com/files/Account/161486/ProfilePicture/2018/02/User_161486_Profile_Picture_20180226-031446_e41b6655-384c-4ca3-93e4-ab1dffb099e0.jpg"
+        title="Toguro"
+      />
+      <Story
+        image="https://image.winudf.com/v2/image/Y29tLmFuZHJvbW8uZGV2NjYwNjE0LmFwcDc0NzgwNF9zY3JlZW5fMF8xNTIyMDY0NjIwXzAxNQ/screen-0.jpg?fakeurl=1&type=.jpg"
+        profileSrc="https://i.pinimg.com/originals/70/df/84/70df84159c05666dc72d0d4d6d2b575e.jpg"
+        title="Son Goku"
+      />
+      <Story
+        image="https://i.pinimg.com/originals/c6/99/53/c69953832f3e770cb784fab9bbc3aa22.jpg"
+        profileSrc="https://avatarfiles.alphacoders.com/126/126710.png"
+        title="Vegeta"
+      />
+      <Story
+        image="https://i.pinimg.com/736x/79/04/e5/7904e5a8d67b5c082146ccb9fc18c967.jpg"
+        profileSrc="https://i.ytimg.com/vi/HYoI2KAFbnM/hqdefault.jpg"
+        title="Naruto"
+      />
+      <Story
+        image="https://i.pinimg.com/originals/f8/80/2d/f8802d2162dfda7444f84fcee91190c2.jpg"
+        profileSrc="https://avatarfiles.alphacoders.com/169/169419.png"
+        title="Goham"
+      />
+    </div>
+  )
+}
+
+export default StoryReel
