1bfd5ce86316e6046c4196db8a00cf0d4ad2ec7e
Reubber <reubbers@alu.ufc.br>
2020-09-01 18:02:49 -0300

    export firebase config

--- facebook/src/firebase.js
+++ facebook/src/firebase.js
@@ -1,3 +1,5 @@
+import firebase from 'firebase'
+
 const firebaseConfig = {
   apiKey: "AIzaSyBocpo4gcUslCFnOQHWFpe82MC_3RdEPOE",
   authDomain: "facebook-clone-5f273.firebaseapp.com",
@@ -8,3 +10,12 @@ const firebaseConfig = {
   appId: "1:223632671638:web:20f0194212ea2ebe5fd3b7",
   measurementId: "G-89PBYF3KMN"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig)
+const db = firebaseApp.firestore()
+const auth = firebase.auth()
+const provider = new firebase.auth.GoogleAuthProvider()
+
+export { auth, provider }
+
+export default db
