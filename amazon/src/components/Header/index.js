f02c3f5fce1ee7c1956a503059ebaf81062c1a9b
Reubber <reubbers@alu.ufc.br>
2020-09-08 17:16:10 -0300

    add header componnent style

---
+++ amazon/src/components/Header/index.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import './styles.css'
+import SearchIcon from '@material-ui/icons/Search'
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket'
+
+function Header() {
+  return (
+    <div className='header'>
+      <img className="header__logo" 
+        src="https://logodownload.org/wp-content/uploads/2014/04/amazon-logo-2.png"
+      />
+
+      <div className="header__search">
+        <input
+          type="text"
+          className="header__searchInput"
+        />
+        <SearchIcon className="header__searchIcon" />
+      </div>
+      
+      <div className="header__nav">
+        <div className="header__option">
+          <span
+            className="header__optionLineOne">
+              Hello Guest
+          </span>
+          <span
+            className="header__optionLineTwo">
+              Sigh IN
+          </span>
+        </div>
+        
+        <div className="header__option">
+          <span
+            className="header__optionLineOne">
+              Returns
+          </span>
+          <span
+            className="header__optionLineTwo">
+              &Orders
+          </span>
+        </div>
+
+        <div className="header__option">
+          <span
+            className="header__optionLineOne">
+              Your
+          </span>
+          <span
+            className="header__optionLineTwo">
+              Prime
+          </span>
+        </div>
+
+        <div className="header__optionBasket">
+          <ShoppingBasketIcon />
+          <span className="header__optionLineTwo header__basketCount">0</span>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Header
