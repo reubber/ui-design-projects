c09950538555421f48bc1024aa6b64052ef16fc7
Reubber <reubbers@alu.ufc.br>
2020-09-08 17:15:47 -0300

    add firebase config

---
+++ amazon/src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdggALQYj2Ih50jS8Tq7FAN9wfMczYbHI",
+  authDomain: "project-42ee2.firebaseapp.com",
+  databaseURL: "https://project-42ee2.firebaseio.com",
+  projectId: "project-42ee2",
+  storageBucket: "project-42ee2.appspot.com",
+  messagingSenderId: "1080134218422",
+  appId: "1:1080134218422:web:6b25a7c0ed70eb17916c17",
+  measurementId: "G-WVECEK5JL7"
+};
